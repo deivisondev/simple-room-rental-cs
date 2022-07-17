28c857a32c44dbc384a66ada27e21985b3403a3e
deivisondev <deivisoneditordevideos@gmail.com>
2022-07-17 16:41:15 -0300

    Início

---
+++ SimpleRoomRental/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace SimpleRoomRental
+{
+    class Program
+    {
+        static void Main()
+        {
+            Room[] rooms = new Room[10];
+
+            Console.Write("How many rooms will be rented? ");
+            int rented = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < rented; i++)
+            {
+                Console.WriteLine($"\nRent #{i + 1}:");
+
+                Console.Write("Nome: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+
+                Console.Write("Room: ");
+                int roomNumber = int.Parse(Console.ReadLine());
+
+                rooms[roomNumber] = new Room(name, email, roomNumber);
+            }
+
+            Console.WriteLine("\nBusy rooms:");
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (rooms[i] != null)
+                    Console.WriteLine(rooms[i]);
+            }
+        }
+    }
+}
---
+++ SimpleRoomRental/Room.cs
@@ -0,0 +1,21 @@
+﻿namespace SimpleRoomRental
+{
+    public class Room
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int RoomNumber { get; private set; }
+
+        public Room(string name, string email, int roomNumber)
+        {
+            Name = name;
+            Email = email;
+            RoomNumber = roomNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"{RoomNumber}: {Name}, {Email}";
+        }
+    }
+}
